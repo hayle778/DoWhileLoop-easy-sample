b58bad19c2392d9f6ae1264d2cd0167cf8317d19
yourUsername <yourEmail@domain.com>
2020-01-26 19:23:15 -0500

    cc

---
+++ src/main/java/com/example/demo/forten.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+public class forten {
+    public static void main(String[] args) {
+
+        int y = 8;
+        do {
+            y += 6;
+
+            System.out.println(""+ y);
+        }
+            while (y <= 25);
+
+    }
+
+    }
+
+
